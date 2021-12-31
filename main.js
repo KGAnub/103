ae0a1cefb560c6b0eac058bd04fc80ceeec96773
KGAnub <87268921+KGAnub@users.noreply.github.com>
2021-12-31 20:33:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-https://teachablemachine.withgoogle.com/models/ach4E8WfN/
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+    });
+    
+    camera = document.getElementById("camera");
+    
+    webcam.attach( '#camera' );
+    
+    function take_snapshot()
+    {
+        Webcam.snap(function(data_url) {
+            document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        });
+    }
+    
+    console.log('ml5 version:',ml5.version);
+    
+    classifier = ml5.imageclassifier('https://teachablemachine.withgoogle.com/models/J3l_8vVaO/',modelLoaded);
